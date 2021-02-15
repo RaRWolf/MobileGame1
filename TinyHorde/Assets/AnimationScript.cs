e51e2973a70f9c47e6ae19e358998a118da003af
Anthony DeCenzo <9rarwolf9@gmail.com>
2021-02-14 20:58:09 -0500

    Added Unity Project
    This version includes a zombie prefab that runs towards where you're holding the mouse or pressing your finger on mobile.

---
+++ TinyHorde/Assets/AnimationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    Animator m_Animator;
+
+    bool m_IsMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_IsMoving = false;
+        m_Animator.SetFloat("Rando",Random.Range(0.0f,1f));
+        m_Animator.Play(0, -1, Random.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            m_Animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            m_Animator.SetBool("IsMoving", false);
+        }
+    }
+}
---
+++ TinyHorde/Assets/ZombieMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ZombieMove : MonoBehaviour
+{
+
+    Vector3 newPosition;
+    Vector3 newPositionLimited;
+
+    //Vector3 newPositionLimited2;
+    public float speed = 1.0f;
+
+
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+    void Update()
+    {
+        //While the mouse is held
+        if (Input.GetMouseButton(0))
+        {
+            //Send out a raycast
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                newPosition = hit.point;
+            }
+
+            //Remove Y from the raycast
+            newPositionLimited = new Vector3(newPosition.x, transform.position.y, newPosition.z);
+
+            //Move towards this ^^
+            transform.position = Vector3.MoveTowards(transform.position, newPositionLimited, Time.deltaTime * speed);
+
+            //Rotate towards the raycast, not including Y.
+            var lookPos = transform.position - newPosition;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * speed);
+        }
+
+    }
+}
